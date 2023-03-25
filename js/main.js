1cd1b9e9b22a756d89f201106b1f7e88d7256d64
Aryan Gupta <94754702+aryanploxxx@users.noreply.github.com>
2023-03-26 00:46:33 +0530

    updated hackathon dates, main.js (#132)
    * corrected logo, deleted extra files in img folder
    
    * corrected email
    
    * changed favicon
    
    * updated hackathon dates, main.js

--- js/main.js
+++ js/main.js
@@ -106,18 +106,4 @@ else {
  buttontext.innerHTML = "Show Less";
 }
 }
-var ctfclick=0;
-var logo=document.getElementById("osdhack");
-logo.addEventListener("click",function ctfeasteregg(){
 
-ctfclick++;
-if(ctfclick==2){
-alert("You are getting closer to the secret🤫")
-}
-if(ctfclick==3)
-{
-  logo.href="https://ctf.osdhackjiit.com/"
-  logo.target = "_blank"
-} 
-}
-)
